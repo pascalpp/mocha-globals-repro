2aa8865180c0570883f078721f2c28774ad30023
Pascal <pascal@pascal.com>
2019-05-13 18:01:02 -0400

    removed unnecessary code

--- index.js
+++ index.js
@@ -7,19 +7,4 @@ const mochaInstance = new Mocha({
 
 mochaInstance.addFile('./test.js');
 
-const runner = mochaInstance.run(function(failures) {
-  // process.send({ message: 'end', failures });
-  process.exitCode = 0; // parent will decide exitCode
-});
-
-// runner.on('pass', () => {
-//   process.send({ message: 'pass' });
-// });
-
-// runner.on('fail', () => {
-//   process.send({ message: 'fail' });
-// });
-
-// runner.on('pending', () => {
-//   process.send({ message: 'pending' });
-// });
+mochaInstance.run();
